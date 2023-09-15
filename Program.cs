759eff525ec4b1820789e55bce9ab86f8bcaf312
ozgurdenizyusuf <yusuftuncdemir@ogr.iu.edu.tr>
2023-09-15 09:02:08 +0300

    Думатель
    Думатель

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Задайте мне вопрос:");
+while (true)
+{
+    string soru = Console.ReadLine();
+    string cevap = "Ответ: 42";
+    Console.WriteLine(cevap);
+    Console.WriteLine("Задайте еще один вопрос:");
+}
